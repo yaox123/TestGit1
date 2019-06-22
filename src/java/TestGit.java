e38a1022934d3e64caa9434391def90c8fc84a52
yaox123 <974526541@qq.com>
2019-06-22 19:37:34 +0800

    第一次测试

---
+++ src/java/TestGit.java
@@ -0,0 +1,15 @@
+package java;
+
+/**
+ * @BelongsProject: TestGit
+ * @BelongsPackage: java
+ * @Author: yaox
+ * @CreateTime: 2019-06-22 19:35
+ */
+public class TestGit {
+
+    /**
+     * 姓名
+     */
+    private String name;
+}
